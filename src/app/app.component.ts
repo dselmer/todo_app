597eda694614566ce2a5edc86bd888d8a59acbfe
dselmer <dselmer@icstars.org>
2023-03-09 19:42:38 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo_app';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: true },
+    { description: 'laugh', done: false },
+  ];
+
+  get item(){
+    if (this.filter === 'all'){
+      return this.allItems
+    }
+    return this.allItems.filter(item => item.done === true ? item.done:!item.done)
+  }
 }
+
+
